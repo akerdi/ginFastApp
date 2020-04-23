112eb03a8cfcc1f7c51790c27df8c3bcbab4d162
shaohung <767838865@qq.com>
2020-04-23 21:56:13 +0800

    hmmm

---
+++ app/app.go
@@ -0,0 +1,5 @@
+package app
+
+func InitApp() int64 {
+	return 89
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"app/app"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("#############", app.InitApp())
+}
