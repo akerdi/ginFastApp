25ee1eac1e89ee212086ed1617677994e65e3507
少鸿 江 <tianxiaoxin001@gmail.com>
2020-04-24 20:29:51 +0800

    hmmm

--- app.go
+++ app.go
@@ -1,9 +1,20 @@
 package ginFastApp
 
+import "github.com/gin-gonic/gin"
+
 func InitApp() int64 {
 	return 89
 }
 
 func AhuangName() string {
 	return  "ahuang"
+}
+
+func Init() *gin.Engine {
+	g := gin.New()
+	return  g
+}
+
+func Start(g *gin.Engine, listenAddress string) error {
+	return g.Run(listenAddress)
 }
