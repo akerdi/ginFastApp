57b6c2d879ccda7654ed231025f654a03828e851
shaohung <767838865@qq.com>
2020-06-09 00:01:53 +0800

    server ConnectDB Start

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 const DRIVER = "mysql"
@@ -19,12 +20,12 @@ type IConfig interface {
 }
 
 type App struct {
-	Config *IConfig
+	Config IConfig
 	beforeMiddlewares []interface{}
 	afterMiddlewares []interface{}
 }
 
-func New(config *IConfig)*App {
+func New(config IConfig)*App {
 	return &App{Config:config}
 }
 
@@ -35,44 +36,34 @@ func (app *App) addAfterMiddleware(middleware interface {})  {
 	app.afterMiddlewares = append(app.afterMiddlewares, middleware)
 }
 
-func (app *App) connectDB(cb func(db *gorm.DB, err error)) {
-	dbMapper := (*app.Config).GetDB()
+func (app *App) ConnectDB(cb func(db *gorm.DB, err error)) {
+	dbMapper := app.Config.GetDB()
 	if dbMapper == nil {
 		cb(nil, errors.New("Config getDB fail"))
 		return
 	}
-	fmt.Println("------", dbMapper)
-	username, ok := dbMapper["DB_Username"].(string)
-	if !ok {
-		cb(nil, errors.New("DB_Username is not ok"))
-		return
+	var username, password, host, name string
+	var port int64
+	for field, val := range dbMapper {
+		if field == "username" {
+			username = val.(string)
+		} else if field == "password" {
+			password = val.(string)
+		} else if field == "host" {
+			host = val.(string)
+		} else if field == "name" {
+			name = val.(string)
+		} else if field == "port" {
+			port = int64(val.(float64))
+		}
 	}
-	password, ok := dbMapper["DB_Password"].(string)
-	if !ok {
-		cb(nil, errors.New("DB_Password is not ok"))
-		return
-	}
-	host, ok := dbMapper["DB_Host"].(string)
-	if !ok {
-		cb(nil, errors.New("DB_Host is not ok"))
-		return
-	}
-	port, ok := dbMapper["DB_Port"].(int64)
-	if !ok {
-		cb(nil, errors.New("DB_Port is not ok"))
-		return
-	}
-	name, ok := dbMapper["DB_Name"].(string)
-	if !ok {
-		cb(nil, errors.New("DB_Name is not ok"))
-		return
-	}
-	fmt.Println("=-====", username, password, host, port, name)
-	return
+	fmt.Println("connect db env: ", username, password, host, port, name)
 	DSN := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, name)
+	fmt.Println("connect db DSN", DSN)
 	var err error
 	db, err = gorm.Open(DRIVER, DSN)
 	if err != nil {
+		fmt.Println("--------connect db err::", err)
 		cb(nil, err)
 		return
 	}
@@ -82,8 +73,8 @@ func (app *App) connectDB(cb func(db *gorm.DB, err error)) {
 // Start 服务器正式启动开始
 func (app *App) Start() (*gin.Engine, error)  {
 	engine := gin.Default()
-	port := (*app.Config).GetPort()
-	portStr := fmt.Sprintf("%d", port)
+	port := app.Config.GetPort()
+	portStr := fmt.Sprintf(":%d", port)
 	err := engine.Run(portStr)
 	if err != nil {
 		return nil, err
