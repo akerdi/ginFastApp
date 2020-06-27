acd7bd6ac368b650d08d4456949a2252b74d2514
shaohung <767838865@qq.com>
2020-06-27 12:36:12 +0800

    support redis

--- connectDB.go
+++ connectDB.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/go-redis/redis"
 )
 
 const DRIVER = "mysql"
 var db *gorm.DB
+
+type RedisClient struct {
+	client *redis.Client
+}
 
 func (app *App) ConnectDB(cb func(db *gorm.DB, err error)) {
 	dbMapper := app.Config.GetDB()
@@ -42,4 +47,48 @@ func (app *App) ConnectDB(cb func(db *gorm.DB, err error)) {
 		return
 	}
 	cb(db, nil)
+}
+
+func (app *App) ConnectRedis(cb func(redisClient *RedisClient, err error))  {
+	redisMapper := app.Config.GetRedis()
+	if redisMapper == nil {
+		cb(nil, errors.New("Config getDB fail"))
+		return
+	}
+	var redisHost, redisPass string
+	var redisPort int64
+	var redisIndex int
+	for field, val := range redisMapper {
+		if field == "host" {
+			redisHost = val.(string)
+		} else if field == "pass" {
+			redisPass = val.(string)
+		} else if field == "port" {
+			redisPort = int64(val.(float64))
+		} else if field == "db_index" {
+			redisIndex = int(val.(float64))
+		} else {
+			fmt.Printf("%s:%s not support", field, val)
+		}
+	}
+	addr := fmt.Sprintf("%s:%d", redisHost, redisPort)
+	redisOptions := redis.Options{
+		Addr : addr,
+		DB : redisIndex,
+	}
+	if redisPass != "" {
+		redisOptions.Password = redisPass
+	}
+	client := redis.NewClient(&redisOptions)
+	redisConnect := &RedisClient{
+		client: client,
+	}
+	pong, err := client.Ping().Result()
+	if err != nil {
+		fmt.Println("redis client meet error : ", err)
+		cb(nil, err)
+		return
+	}
+	fmt.Println("redis client set up successfully! pong: ", pong)
+	cb(redisConnect, nil)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/shaohung001/ginFastApp
 go 1.13
 
 require (
-	github.com/gin-contrib/gzip v0.0.1 // indirect
-	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607 // indirect
-	github.com/gin-gonic/gin v1.6.2 // indirect
-	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/gin-contrib/gzip v0.0.1
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.6.2
+	github.com/go-redis/redis v6.15.8+incompatible
+	github.com/jinzhu/gorm v1.9.12
+	github.com/onsi/ginkgo v1.13.0 // indirect
 )
